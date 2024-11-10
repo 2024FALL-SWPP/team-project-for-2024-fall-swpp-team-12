aa6d7654a02a921e2172149fd7c0cc07a9da561e
GyubinKimProg <kebinxyz@snu.ac.kr>
2024-11-10 22:21:38 +0900

    F-009-1 in progress (comment, logic correction, state for phantom, etc)

--- Chronus/Assets/Scripts/InputManager.cs
+++ Chronus/Assets/Scripts/InputManager.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 public class InputManager : MonoBehaviour
 {
     public static InputManager inputManager; // singleton
-    public Action<string> OnInput; // to PlayerController.HandleInput()
-    public Action<string> OnTimeRewindInput; // to PlayerController.HandleTimeRewindInput()
-    public Action OnSpace; // to PlayerController.ToggleTimeRewind()
+    public Action<string> OnCommand; // to PlayerController.HandleMovementInput()
+    public Action OnTimeRewindModeToggle; // to PlayerController.ToggleTimeRewind()
+    public Action<string> OnTimeRewindControl; // to PlayerController.HandleTimeRewindInput()
 
     private void Awake()
     {
@@ -17,35 +17,35 @@ public class InputManager : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.W))
         {
-            OnInput?.Invoke("w");
+            OnCommand?.Invoke("w");
         }
         else if (Input.GetKeyDown(KeyCode.S))
         {
-            OnInput?.Invoke("s");
+            OnCommand?.Invoke("s");
         }
         else if (Input.GetKeyDown(KeyCode.A))
         {
-            OnInput?.Invoke("a");
+            OnCommand?.Invoke("a");
         }
         else if (Input.GetKeyDown(KeyCode.D))
         {
-            OnInput?.Invoke("d");
+            OnCommand?.Invoke("d");
         }
         else if (Input.GetKeyDown(KeyCode.R)) 
         {
-            OnInput?.Invoke("r");
+            OnCommand?.Invoke("r");
         }
         else if (Input.GetKeyDown(KeyCode.Space)) 
         {
-            OnSpace?.Invoke();
+            OnTimeRewindModeToggle?.Invoke();
         }
         else if (Input.GetKeyDown(KeyCode.Q)) 
         {
-            OnTimeRewindInput?.Invoke("q");
+            OnTimeRewindControl?.Invoke("q");
         }
         else if (Input.GetKeyDown(KeyCode.E))
         {
-            OnTimeRewindInput?.Invoke("e");
+            OnTimeRewindControl?.Invoke("e");
         }
     }
 }
---
+++ Chronus/Assets/Scripts/Phantom/PhantomIdle.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhantomIdle : MonoBehaviour, IState<PhantomController>
+{
+    private PhantomController _playerController;
+
+    // State Replace!
+    public void OperateEnter(PhantomController sender)
+    {
+        _playerController = sender;
+        if (_playerController != null)
+        {
+            _playerController.curSpeed = 0;
+        }
+    }
+
+    // State be Replaced by others
+    public void OperateExit(PhantomController sender)
+    {
+
+    }
+
+    // Always Do something when the current state is this state
+    public void OperateUpdate(PhantomController sender)
+    {
+        //need "fall" condition
+        //game over by fell condition also.
+    }
+    public void DoneAction(PhantomController sender)
+    {
+        _playerController.doneAction = true;
+    }
+}
