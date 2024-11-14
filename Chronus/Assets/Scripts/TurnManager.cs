37d1c0ef4ca18cbe888cf04c5ecbb39c91377ce3
GyubinKimProg <kebinxyz@snu.ac.kr>
2024-11-14 19:19:20 +0900

    button: bug fix, init count when ON again or keep ON, turnmanager fix
    about turn manager fix: made new bool type variable for base clock state(named 'idleClock'), when do nothing: that clock is on
    and made new bool type variable for whole clock (named 'CLOCK'), encapsuling whole sub clocks
    renamed UpdateTurn -> NextSubClock

--- Chronus/Assets/Scripts/TurnManager.cs
+++ Chronus/Assets/Scripts/TurnManager.cs
@@ -5,13 +5,19 @@ using UnityEngine;
 public class TurnManager : MonoBehaviour
 {
     public static TurnManager turnManager;
-    public int turn = 0; 
+
+    public int turn = 0;
+
+    public bool CLOCK = false;
+
+    public bool idleClock = true;
     public bool turnClock = false;
     public bool firstCollisionCheck = false;
     public bool lateTurnClock = false;
     public bool secondCollisionCheck = false;
     public bool fallClock = false;
     public bool lastCollisionCheck = false;
+
     // removed object control for simplicity: this will be done in other branch.
 
     public Dictionary<string, bool> dicTurnCheck;
@@ -32,44 +38,51 @@ public class TurnManager : MonoBehaviour
 
     void Update()
     {
-        if (turnClock && dicTurnCheck["Player"] && (dicTurnCheck["Phantom"] || !PlayerController.playerController.isPhantomExists)/* && and so on...*/) //action execution (player, phantom, switches, objects, etc)
+        if (CLOCK) //whole.
         {
-            UpdateTurn(ref turnClock, ref firstCollisionCheck);
-            dicTurnCheck["Player"] = false;
-            dicTurnCheck["Phantom"] = false;
-        }
-        if (firstCollisionCheck && dicTurnCheck["Button"]/* && something*/) //collision check between player and harmful objects (first) (if collide, kill)
-        {
-            UpdateTurn(ref firstCollisionCheck, ref lateTurnClock);
-            dicTurnCheck["Button"] = false;
-            //dicTurnCheck ....... = false;
-        }
-        if (lateTurnClock/* && something*/) //late action execution (moving wall, block, spear, etc)
-        {
-            UpdateTurn(ref lateTurnClock, ref secondCollisionCheck);
-            //dicTurnCheck ....... = false;
-        }
-        if (secondCollisionCheck/* && something*/) //collision check between player and harmful objects (second) (if collide, kill)
-        {
-            UpdateTurn(ref secondCollisionCheck, ref fallClock);
-            //dicTurnCheck ....... = false;
-        }
-        if (fallClock/* && something*/) //make them fall (land, or kill)
-        {
-            UpdateTurn(ref fallClock, ref lastCollisionCheck);
-            //dicTurnCheck ....... = false;
-        }
-        if (lastCollisionCheck/* && something*/) //collision check between player and harmful objects (last) (if collide, kill)
-        {
-            lastCollisionCheck = false;
-            //dicTurnCheck ....... = false;
-            turn++;
-            //PlayerController.playerController.listPosLog.Add((PlayerController.playerController.playerCurPos, PlayerController.playerController.playerCurRot)); //position tracking log update!
-        }
+            if (idleClock)
+            {
+                NextSubClock(ref idleClock, ref turnClock);
+            }
+            if (turnClock && dicTurnCheck["Player"] && (dicTurnCheck["Phantom"] || !PlayerController.playerController.isPhantomExists)/* && and so on...*/) //action execution (player, phantom, switches, objects, etc)
+            {
+                NextSubClock(ref turnClock, ref firstCollisionCheck);
+                dicTurnCheck["Player"] = false;
+                dicTurnCheck["Phantom"] = false;
+            }
+            if (firstCollisionCheck && dicTurnCheck["Button"]/* && something*/) //collision check between player and harmful objects (first) (if collide, kill)
+            {
+                NextSubClock(ref firstCollisionCheck, ref lateTurnClock);
+                dicTurnCheck["Button"] = false;
+                //dicTurnCheck ....... = false;
+            }
+            if (lateTurnClock/* && something*/) //late action execution (moving wall, block, spear, etc)
+            {
+                NextSubClock(ref lateTurnClock, ref secondCollisionCheck);
+                //dicTurnCheck ....... = false;
+            }
+            if (secondCollisionCheck/* && something*/) //collision check between player and harmful objects (second) (if collide, kill)
+            {
+                NextSubClock(ref secondCollisionCheck, ref fallClock);
+                //dicTurnCheck ....... = false;
+            }
+            if (fallClock/* && something*/) //make them fall (land, or kill)
+            {
+                NextSubClock(ref fallClock, ref lastCollisionCheck);
+                //dicTurnCheck ....... = false;
+            }
+            if (lastCollisionCheck/* && something*/) //collision check between player and harmful objects (last) (if collide, kill)
+            {
+                NextSubClock(ref lastCollisionCheck, ref idleClock);
+                //dicTurnCheck ....... = false;
+                CLOCK = false;
 
+                turn++;
+            }
+        }
     }
 
-    private void UpdateTurn(ref bool previousClock, ref bool nextClock)
+    private void NextSubClock(ref bool previousClock, ref bool nextClock)
     {
         previousClock = false;
         nextClock = true;
