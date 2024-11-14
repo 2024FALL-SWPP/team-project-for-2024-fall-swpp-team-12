774555cd3263376ad64bfdcab02105e6ca6da6e1
Lee Seunghyeon <lsh09130@gmail.com>
2024-11-14 21:21:21 +0900

    adding laser (with interactions)
    almost done, without toggling by button/lever, because it's largely changed at F-022, so I'll do afterwards.

---
+++ Chronus/Assets/Scripts/Laser.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    public float laserSpeed = 20f; 
+    public float laserLength = 100f; // arbitrary maximum length
+    public LayerMask collisionLayer; 
+    private LineRenderer lineRenderer; 
+
+    void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.positionCount = 2; // start and end point
+        lineRenderer.startWidth = 0.1f;
+        lineRenderer.endWidth = 0.1f; // 0.05f;
+    }
+
+    public void ToggleLaser()
+    {
+        // turn off if on, turn on if off
+        if (gameObject.activeSelf) {gameObject.SetActive(false);}
+        else {gameObject.SetActive(true);}
+    }
+
+    void Update()
+    {
+        CastLaser();
+    }
+
+    void CastLaser()
+    {
+        Vector3 start = transform.position;
+        Vector3 direction = transform.forward;
+
+        if (Physics.Raycast(start, direction, out RaycastHit hit, Mathf.Infinity, collisionLayer))
+        {
+            // stop at the collision(hit) point 
+            lineRenderer.SetPosition(0, start);
+            lineRenderer.SetPosition(1, hit.point);
+            if (hit.collider.CompareTag("Player"))
+            {
+                Debug.Log("Game Over!");
+            }
+        }
+        else
+        {
+            // if there's no collision, it goes infinitely 
+            lineRenderer.SetPosition(0, start);
+            lineRenderer.SetPosition(1, start + direction * laserLength);
+        }
+    }
+}
